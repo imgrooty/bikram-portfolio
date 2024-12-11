fa235a8d6daf160ff7c885f261cc67e73229ab8b
Grooty <us08692@gmail.com>
2024-12-11 16:02:40 +0545

    ignore eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        // Ignore ESLint during production builds
+        ignoreDuringBuilds: true,
+      },
+};
 
 export default nextConfig;
